8ab9d973a68ab12b93985ade8ba376abb82e3fd4
brumens <brumens@protonmail.com>
2024-03-23 01:08:41 +0100

    Dev - UI checkpoint

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -6,18 +6,17 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/lipgloss/table"
 	"golang.org/x/term"
 )
 
-var (
+const (
 	AUTHOR             = "By: @yeswehack / Brumens"
 	TOOL               = "FireFly (v1.3.1)"
-	BACKGROUND_PATTERN = "萤火虫"
 	MODE_DARK          = "#383838"
 	MODE_LIGHT         = "#D9DCCF"
-	BORDER             = lipgloss.NormalBorder()
+	BACKGROUND_PATTERN = "萤火虫"
 
 	COLOR_BLACK  = lipgloss.Color("#000000")
 	COLOR_WHITE  = lipgloss.Color("#D9DCCF")
@@ -29,28 +28,31 @@ var (
 )
 
 type TerminalUI struct {
-	Result
-	Style   *style
-	spinner string
+	Data
+	Style              *style
+	listPayload        list.Model
+	listTransformation list.Model
+	spinner            string
+	window             int
 }
 
 type style struct {
-	item                lipgloss.Style
-	core                lipgloss.Style
-	banner              lipgloss.Style
-	author              lipgloss.Style
-	column              lipgloss.Style
-	payload             lipgloss.Style
-	box                 lipgloss.Style
-	bar                 lipgloss.Style
-	done                lipgloss.Style
-	table               lipgloss.Style
-	spinner             lipgloss.Style
-	tablePayload        *table.Table
-	tableTransformation *table.Table
-	adColor             lipgloss.AdaptiveColor
-	oversize            func(s ...string) string
-	header              func(s ...string) string
+	item         lipgloss.Style
+	core         lipgloss.Style
+	banner       lipgloss.Style
+	author       lipgloss.Style
+	column       lipgloss.Style
+	payload      lipgloss.Style
+	infobox      lipgloss.Style
+	bar          lipgloss.Style
+	done         lipgloss.Style
+	table        lipgloss.Style
+	spinner      lipgloss.Style
+	window       lipgloss.Style
+	windowActive lipgloss.Style
+	adColor      lipgloss.AdaptiveColor
+	oversize     func(s ...string) string
+	header       func(s ...string) string
 	width
 }
 
@@ -60,37 +62,29 @@ type width struct {
 	columnMid   int
 }
 
-func NewTerminalUI() TerminalUI {
-	return TerminalUI{
-		Style: newStyle(),
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+func NewTerminalUI() *TerminalUI {
+	demoList := []list.Item{
+		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
+	}
+
+	return &TerminalUI{
+		Style:              NewStyle(),
+		listPayload:        list.New(demoList, list.NewDefaultDelegate(), 0, 0),
+		listTransformation: list.New(demoList, list.NewDefaultDelegate(), 0, 0),
 	}
 }
 
-/*
-func makeTable() {
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(7),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
-}
-*/
-
 // Make style definitions for the terminal user-interface (UI)
-func newStyle() *style {
+func NewStyle() *style {
+	border := lipgloss.NormalBorder()
 	adaptiveColor := lipgloss.AdaptiveColor{Light: MODE_LIGHT, Dark: MODE_DARK}
 	s := &style{
 		adColor: adaptiveColor,
@@ -101,12 +95,13 @@ func newStyle() *style {
 		banner:  lipgloss.NewStyle().Foreground(COLOR_YELLOW),
 		payload: lipgloss.NewStyle().Foreground(COLOR_ORANGE),
 		author:  lipgloss.NewStyle().Foreground(COLOR_WHITE),
+
 		done: lipgloss.NewStyle().
 			Padding(0, 1, 0, 1).
 			SetString("✓").
 			Foreground(COLOR_GREEN),
 
-		box: lipgloss.NewStyle().
+		infobox: lipgloss.NewStyle().
 			Padding(0, 1, 0, 1).
 			Background(COLOR_WHITE).
 			Foreground(COLOR_BLACK),
@@ -120,51 +115,120 @@ func newStyle() *style {
 			Bold(true).
 			Render,
 
-		column: lipgloss.NewStyle().
-			Align(lipgloss.Left).
-			Border(BORDER, false, true, true, false).
-			BorderForeground(adaptiveColor).
-			Height(20),
-
 		table: lipgloss.NewStyle().
 			Foreground(COLOR_GREY).
+			Height(20),
+
+		window: lipgloss.NewStyle().
+			Width(15).
+			Height(5).
+			Align(lipgloss.Center, lipgloss.Center).
+			BorderStyle(lipgloss.HiddenBorder()),
+
+		windowActive: lipgloss.NewStyle().
+			Width(15).
+			Height(5).
+			Align(lipgloss.Center, lipgloss.Center).
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("69")),
+
+		column: lipgloss.NewStyle().
+			Align(lipgloss.Left).
+			Border(border, false, true, true, false).
+			BorderForeground(adaptiveColor).
 			Height(20),
 	}
 
 	return s
 }
 
-func makeTable(headers []string, style ...lipgloss.Style) *table.Table {
-	t := table.New().
-		Headers(headers...).
-		Border(lipgloss.ThickBorder()).
-		BorderStyle(
-			lipgloss.NewStyle().
-				Foreground(COLOR_GREY),
+func (t *TerminalUI) Render(r Data) string {
+	s := t.Style
+	screenWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
+	s.width.setColumns(screenWidth)
+
+	// Prepare banner
+	banner := t.banner(screenWidth)
+
+	//Get the process time
+	timerDuration := r.stats.GetTime()
+	timer := fmt.Sprintf("%d:%02d:%02d", timerDuration[0], timerDuration[1], timerDuration[2])
+
+	//	columnWidth := screenWidth / 2
+	stringUI := strings.Builder{}
+
+	// Add the banner at the top
+	stringUI.WriteString(banner + "\n")
+
+	//Make all the columns
+	{
+		columnLeft := s.column.Render(
+			s.HeaderRender(lipgloss.Center, "Analyzer", s.width.columnleft) +
+				lipgloss.JoinVertical(lipgloss.Left,
+					s.viewItem("Hits........", 0),
+					s.viewItem("Chars.......", 0),
+					s.viewItem("Patterns....", 0),
+					s.viewItem("Mutations...", 0),
+					s.viewItem("Reflections.", 0),
+				) + "\n" +
+				s.HeaderRender(lipgloss.Center, "Scanner", s.width.columnleft) +
+				lipgloss.JoinVertical(lipgloss.Left,
+					s.viewItem("Behavior...", r.stats.Behavior.GetCount()),
+					s.viewItem("Difference.", r.stats.Difference.GetCount()),
+					s.viewItem("Transform..", r.stats.Transformation.GetCount()),
+				) + "\n" +
+				s.HeaderRender(lipgloss.Center, "Request", s.width.columnleft) +
+				lipgloss.JoinVertical(lipgloss.Left,
+					s.viewItem("Requests.....", r.stats.Request.GetCount()),
+					s.viewItem("Responses....", r.stats.Response.GetCount()),
+					s.viewItem("Filtered.....", r.stats.Request.GetFilterCount()),
+					s.viewItem("Forbidden....", r.stats.Request.GetCountForbidden()),
+					s.viewItem("Request Err..", r.stats.Request.GetErrorCount()),
+					s.viewItem("Response Err.", r.stats.Response.GetErrorCount()),
+					s.viewItem("Average Time.", r.stats.Response.GetAverageTime()),
+				),
 		)
-	// Custom style
-	if len(style) == 0 {
-		t.StyleFunc(
-			func(row, col int) lipgloss.Style {
-				return style[0]
-			},
+		columMid := s.column.Render(
+			s.HeaderRender(lipgloss.Center, "Payload", s.width.columnMid) +
+				lipgloss.JoinVertical(lipgloss.Left),
+			//t.listPayload.View(),
 		)
-	} else {
-		t.StyleFunc(
-			func(row, col int) lipgloss.Style {
-				var style lipgloss.Style
-				return style
-			},
+
+		columnRight := s.column.Render(
+			s.HeaderRender(lipgloss.Center, "Transformation", s.width.columnMid) +
+				lipgloss.JoinVertical(lipgloss.Left), //t.listTransformation.View(),
+
+		)
+
+		//Write all columns
+		stringUI.WriteString(
+			lipgloss.JoinHorizontal(lipgloss.Top,
+				lipgloss.JoinHorizontal(
+					lipgloss.Top,
+					columnLeft,
+					columMid,
+					columnRight,
+				),
+			) + "\n",
 		)
 	}
-	return t
-}
 
-func (s *style) TablePayloadAppendRows(rows [][]string) *table.Table {
-	return s.tablePayload.Rows(rows...)
-}
-func (s *style) TableTransformationAppendRows(rows [][]string) *table.Table {
-	return s.tableTransformation.Rows(rows...)
+	// Status bar
+	{
+		statusBar := s.bar.Render(
+			lipgloss.JoinHorizontal(lipgloss.Left,
+				s.infobox.Background(COLOR_GREEN).Render(timer),
+				s.viewItem("Current", r.ResultFinal.Payload, s.payload),
+			),
+		)
+		stringUI.WriteString(statusBar + "\n")
+	}
+
+	if screenWidth > 0 {
+		t.Style.core = t.Style.core.MaxWidth(screenWidth)
+	}
+
+	return t.Style.core.Render(stringUI.String())
 }
 
 func (s *style) HeaderRender(position lipgloss.Position, header string, width int) string {
@@ -178,7 +242,7 @@ func (s *style) HeaderRender(position lipgloss.Position, header string, width in
 	) + "\n"
 }
 
-func (s *style) TableRender(position lipgloss.Position, table string, width int) string {
+/* func (s *style) TableRender(position lipgloss.Position, table string, width int) string {
 	return lipgloss.JoinVertical(position,
 		lipgloss.Place(width, 1,
 			lipgloss.Center, lipgloss.Center,
@@ -187,6 +251,53 @@ func (s *style) TableRender(position lipgloss.Position, table string, width int)
 		),
 	) + "\n"
 }
+*/
+// Set the window that will be in focus
+func (m *TerminalUI) SetSpinner(spinner string) {
+	m.spinner = spinner
+}
+
+func (m *TerminalUI) SetWindow(wid int) {
+	m.window = wid
+}
+
+// Set column width and return the width used from the screen width
+func (w *width) setColumns(screenWidth int) int {
+	sw := screenWidth / 3
+	widthUsed := 0
+
+	//Left column
+	if sw >= 24 {
+		w.columnleft = 24
+		widthUsed += 24
+		sw = (screenWidth - 24)
+	} else {
+		w.columnleft = sw
+		widthUsed += sw
+	}
+	sw = (screenWidth - w.columnleft) / 2
+	w.columnRight = sw
+	w.columnMid = sw
+
+	return widthUsed
+}
+
+// Show the banner in the terminal UI
+func (t *TerminalUI) banner(width int) string {
+	banner := lipgloss.Place(width, 1,
+		lipgloss.Center, lipgloss.Center,
+		t.Style.banner.Render(TOOL),
+		lipgloss.WithWhitespaceChars(BACKGROUND_PATTERN),
+		lipgloss.WithWhitespaceForeground(t.Style.adColor),
+	)
+	author := lipgloss.Place(width, 1,
+		lipgloss.Center, lipgloss.Center,
+		t.Style.author.Render(AUTHOR),
+		lipgloss.WithWhitespaceChars(BACKGROUND_PATTERN),
+		lipgloss.WithWhitespaceForeground(t.Style.adColor),
+	)
+	return banner + "\n" + author
+}
 
 // Take a argument name and a value that represent an unkown value.
 // The value vill be escaped with the 'strconv.Quote()' function to avoid ASNI injections.
@@ -194,9 +305,10 @@ func (s *style) TableRender(position lipgloss.Position, table string, width int)
 // !WARNING! The "name" argument MUST BE TRUSTED!
 func (s *style) viewItem(trustedNameValue string, value any, lipglosStyle ...lipgloss.Style) string {
 	var (
-		v        string
-		oversize string
-		sep      = ":"
+		maxLength = 24
+		v         string
+		oversize  string
+		sep       = ":"
 	)
 
 	/** The reason why we not use the method: fmt.Sprintf("%v", value) first is because
@@ -220,9 +332,9 @@ func (s *style) viewItem(trustedNameValue string, value any, lipglosStyle ...lip
 	}
 
 	// Value are longer than expected then cut it:
-	if len(v) > 20 {
+	if len(v) > maxLength {
 		oversize = "..."
-		v = v[0:20]
+		v = v[0:maxLength]
 	}
 
 	if len(lipglosStyle) > 0 {
@@ -231,152 +343,12 @@ func (s *style) viewItem(trustedNameValue string, value any, lipglosStyle ...lip
 	return s.item.Render(trustedNameValue+sep, v, s.oversize(oversize))
 }
 
-func (t *TerminalUI) banner(width int) string {
-	banner := lipgloss.Place(width, 1,
-		lipgloss.Center, lipgloss.Center,
-		t.Style.banner.Render(TOOL),
-		lipgloss.WithWhitespaceChars(BACKGROUND_PATTERN),
-		lipgloss.WithWhitespaceForeground(t.Style.adColor),
-	)
-	author := lipgloss.Place(width, 1,
-		lipgloss.Center, lipgloss.Center,
-		t.Style.author.Render(AUTHOR),
-		lipgloss.WithWhitespaceChars(BACKGROUND_PATTERN),
-		lipgloss.WithWhitespaceForeground(t.Style.adColor),
-	)
-	return banner + "\n" + author
+func (m *style) payloadView() string {
+	return "test" //spin + info + gap + prog + pkgCount
 }
 
-func (t *TerminalUI) Render(r Result) string {
-	s := t.Style
-	screenWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
-	s.width.setColumns(screenWidth)
-
-	// Prepare banner
-	banner := t.banner(screenWidth)
-
-	//Get the process time
-	timerDuration := r.stats.GetTime()
-	timer := fmt.Sprintf("%d:%02d:%02d", timerDuration[0], timerDuration[1], timerDuration[2])
-
-	//	columnWidth := screenWidth / 2
-	stringUI := strings.Builder{}
-
-	// Add the banner at the top
-	stringUI.WriteString(banner + "\n")
-
-	//Make all the columns
-	{
-		columnLeft := s.column.Render(
-			s.HeaderRender(lipgloss.Center, "Analyzer", s.width.columnleft) +
-				lipgloss.JoinVertical(lipgloss.Left,
-					s.viewItem("Text......", 0),
-					s.viewItem("Words.....", 0),
-					s.viewItem("Tags......", 0),
-					s.viewItem("Attr......", 0),
-					s.viewItem("AttrValue.", 0),
-					s.viewItem("Comments..", 0),
-				) + "\n" +
-				s.HeaderRender(lipgloss.Center, "Scanner", s.width.columnleft) +
-				lipgloss.JoinVertical(lipgloss.Left,
-					s.viewItem("Behavior...", r.stats.Behavior.GetCount()),
-					s.viewItem("Difference.", r.stats.Difference.GetCount()),
-					s.viewItem("Transform..", r.stats.Transformation.GetCount()),
-				) + "\n" +
-				s.HeaderRender(lipgloss.Center, "Request", s.width.columnleft) +
-				lipgloss.JoinVertical(lipgloss.Left,
-					s.viewItem("Requests.....", r.stats.Request.GetCount()),
-					s.viewItem("Responses....", r.stats.Response.GetCount()),
-					s.viewItem("Filtered.....", r.stats.Request.GetFilterCount()),
-					s.viewItem("Forbidden....", r.stats.Request.GetCountForbidden()),
-					s.viewItem("Request Err..", r.stats.Request.GetErrorCount()),
-					s.viewItem("Response Err.", r.stats.Response.GetErrorCount()),
-					s.viewItem("Average Time.", r.stats.Response.GetAverageTime()),
-				),
-		)
-		columMid := s.column.Render(
-			s.HeaderRender(lipgloss.Center, "Payload", s.width.columnMid) +
-				lipgloss.JoinVertical(lipgloss.Left,
-					s.viewItem("Hits........", 0),
-					s.viewItem("Chars.......", 0),
-					s.viewItem("Patterns....", 0),
-					s.viewItem("Mutations...", 0),
-					s.viewItem("Reflections.", 0),
-					s.viewItem("Current.....", r.ResultFinal.Payload, s.payload),
-				),
-		)
-
-		columnRight := s.table.Render(
-			s.tablePayload.Render(),
-		)
-
-		/* columnRight := s.column.Copy().Render(
-			s.HeaderRender(lipgloss.Center, "Transformation", s.width.columnRight)+
-				lipgloss.JoinVertical(lipgloss.Left),
-			s.tablePayload.Render(),
-		) */
-
-		//Write all columns
-		stringUI.WriteString(
-			lipgloss.JoinHorizontal(lipgloss.Top,
-				lipgloss.JoinHorizontal(
-					lipgloss.Top,
-					columnLeft,
-					columMid,
-					columnRight,
-				),
-			) + "\n",
-		)
-	}
-
-	//Bottom status bar
-	{
-		statusBar := s.bar.Render(
-			lipgloss.JoinHorizontal(lipgloss.Left,
-				s.box.Background(COLOR_GREEN).Render(timer),
-			),
-		)
-		stringUI.WriteString(statusBar)
-	}
-
-	if screenWidth > 0 {
-		t.Style.core = t.Style.core.MaxWidth(screenWidth)
-	}
-
-	return t.Style.core.Render(stringUI.String())
-}
-
-// Set column width and return the width used from the screen width
-func (w *width) setColumns(screenWidth int) int {
-	sw := screenWidth / 3
-	widthUsed := 0
-
-	//Left column
-	if sw >= 24 {
-		w.columnleft = 24
-		widthUsed += 24
-		sw = (screenWidth - 24)
-	} else {
-		w.columnleft = sw
-		widthUsed += sw
-	}
-	sw = (screenWidth - w.columnleft) / 2
-	w.columnRight = sw
-	w.columnMid = sw
-
-	//Right column
-	/* if sw >= 50 {
-		w.columnRight = 80
-		w.columnMid = 80
-		widthUsed += 160
-	} else {
-		w.columnRight = sw
-		w.columnMid = sw
-	} */
-	return widthUsed
-}
-
-func (s *style) progressbar(header string, current, max int) string {
+// Update the process bar
+func (s *style) progressBar(header string, current, max int) string {
 	v := header + strconv.Itoa(current/max)
 	if current == max {
 		return s.done.Render(v)
