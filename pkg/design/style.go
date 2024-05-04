537351adf31a72bdf15aedcf72370b10d83cf860
brumens <68474266+Brum3ns@users.noreply.github.com>
2024-05-04 16:43:04 +0200

    New design feature project started

---
+++ pkg/design/style.go
@@ -0,0 +1,27 @@
+package design
+
+import "github.com/charmbracelet/lipgloss"
+
+const (
+	COLOR_RED    = lipgloss.Color("#EB2D3A")
+	COLOR_BLUE   = lipgloss.Color("#0069D9")
+	COLOR_PINK   = lipgloss.Color("#D900C7")
+	COLOR_GREY   = lipgloss.Color("#383838")
+	COLOR_WHITE  = lipgloss.Color("#D9DCCF")
+	COLOR_BLACK  = lipgloss.Color("#000000")
+	COLOR_GREEN  = lipgloss.Color("#3AF191")
+	COLOR_PURPEL = lipgloss.Color("#7F00D9")
+	COLOR_ORANGE = lipgloss.Color("#D98D00")
+	COLOR_YELLOW = lipgloss.Color("#FFDF00")
+)
+
+type Style struct {
+}
+
+func MakeStyle() {
+
+}
+
+func (s Style) StatusCode(v int) {
+
+}
