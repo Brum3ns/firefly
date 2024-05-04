d6726e3954c39ce1c761960ece97bf356b3a5a56
brumens <68474266+Brum3ns@users.noreply.github.com>
2024-05-04 16:40:49 +0200

    Improvd the detection of differences in HTTP responses

--- pkg/httpdiff/httpdiff.go
+++ pkg/httpdiff/httpdiff.go
@@ -1,50 +1,50 @@
 package httpdiff
 
 import (
-	"net/http"
+	"slices"
 
 	"github.com/Brum3ns/firefly/pkg/httpprepare"
 	"github.com/Brum3ns/firefly/pkg/randomness"
 )
 
 type Difference struct {
-	*Properties
+	*Settings
 }
 
-type Properties struct {
+type Settings struct {
 	Payload       string
 	PayloadVerify string
-	*ResponseBody
-	*ResponseHeaders
-	CompareHTMLNodes httpprepare.HTMLNodeCombine
-	//CompareHeaders   httpprepare.Header
+	Compare
 }
 
-type ResponseBody struct {
-	Body     string
-	HtmlNode httpprepare.HTMLNode
+type Compare struct {
+	HeaderMergeNode httpprepare.Header
+	HTMLMergeNode   httpprepare.HTMLNodeCombine
 }
 
-type ResponseHeaders struct {
-	HeaderString string
-	Headers      http.Header
-	HeadersKnown http.Header
+type Result struct {
+	OK bool
+	HeaderResult
+	HTMLResult
 }
 
 type HeaderResult struct {
-	OK     bool
-	header http.Header
+	OK         bool
+	HeaderHits int
+	Appear     httpprepare.Header
+	Disappear  httpprepare.Header
 }
 
 type HTMLResult struct {
-	OK bool
-	HTMLNodeDiff
+	OK        bool
+	Appear    HTMLNodeDiff
+	Disappear HTMLNodeDiff
 }
 
 type HTMLNodeDiff struct {
 	TagStartHits       int
 	TagEndHits         int
-	TagSelfClose       int
+	TagSelfCloseHits   int
 	WordsHits          int
 	CommentHits        int
 	AttributeHits      int
@@ -58,9 +58,9 @@ type diffNode struct {
 	data            map[string]int
 }
 
-func NewDifference(p Properties) *Difference {
+func NewDifference(settings Settings) *Difference {
 	return &Difference{
-		Properties: &p,
+		Settings: &settings,
 	}
 }
 
@@ -71,99 +71,161 @@ func newDiffNode() diffNode {
 }
 
 // Run the [diff]erence enumiration process for the HTML node
-func (diff *Difference) GetHTMLNodeDiff() HTMLResult {
+func (diff *Difference) GetHTMLNodeDiff(htmlNode httpprepare.HTMLNode) HTMLResult {
 	totalHits := 0
-	storage := []diffNode{}
+	storage := struct {
+		appearHits    int
+		disappearHits int
+		appear        []diffNode
+		disappear     []diffNode
+	}{}
 
 	//!Note : Order for "current" and "known" togther with the list length *MUST* be the same:
 	current := [7]diffNode{
-		{data: diff.Properties.HtmlNode.TagStart},
-		{data: diff.Properties.HtmlNode.TagEnd},
-		{data: diff.Properties.HtmlNode.TagSelfClose},
-		{data: diff.Properties.HtmlNode.Words, checkRandomness: true},
-		{data: diff.Properties.HtmlNode.Comment, checkRandomness: true},
-		{data: diff.Properties.HtmlNode.Attribute},
-		{data: diff.Properties.HtmlNode.AttributeValue, checkRandomness: true},
+		{data: htmlNode.TagStart},
+		{data: htmlNode.TagEnd},
+		{data: htmlNode.TagSelfClose},
+		{data: htmlNode.Words, checkRandomness: true},
+		{data: htmlNode.Comment, checkRandomness: true},
+		{data: htmlNode.Attribute},
+		{data: htmlNode.AttributeValue, checkRandomness: true},
 	}
 	known := [7]map[string][]int{
-		diff.Properties.CompareHTMLNodes.TagStart,
-		diff.Properties.CompareHTMLNodes.TagEnd,
-		diff.Properties.CompareHTMLNodes.TagSelfClose,
-		diff.Properties.CompareHTMLNodes.Words,
-		diff.Properties.CompareHTMLNodes.Comment,
-		diff.Properties.CompareHTMLNodes.Attribute,
-		diff.Properties.CompareHTMLNodes.AttributeValue,
+		diff.Settings.Compare.HTMLMergeNode.TagStart,
+		diff.Settings.Compare.HTMLMergeNode.TagEnd,
+		diff.Settings.Compare.HTMLMergeNode.TagSelfClose,
+		diff.Settings.Compare.HTMLMergeNode.Words,
+		diff.Settings.Compare.HTMLMergeNode.Comment,
+		diff.Settings.Compare.HTMLMergeNode.Attribute,
+		diff.Settings.Compare.HTMLMergeNode.AttributeValue,
 	}
 
 	for i := 0; i < len(current); i++ {
 		//Detect difference
-		diffCurrent := nodeDiff(current[i], known[i], diff.Payload)
+		diffAppear, diffDisappear := nodeDiff(current[i], known[i], diff.Payload)
 
-		storage = append(storage, diffCurrent)
-		totalHits += diffCurrent.hit
+		storage.appear = append(storage.appear, diffAppear)
+		storage.appearHits += diffAppear.hit
+
+		storage.disappear = append(storage.disappear, diffDisappear)
+		storage.disappearHits += diffDisappear.hit
+
+		totalHits += (diffAppear.hit + diffDisappear.hit)
 	}
 
 	return HTMLResult{
 		// !Note : The order for this *MUST* follow the same as "known" and "current" above
 		OK: (totalHits > 0),
-		HTMLNodeDiff: HTMLNodeDiff{
-			TagStartHits:       storage[0].hit,
-			TagEndHits:         storage[1].hit,
-			TagSelfClose:       storage[2].hit,
-			WordsHits:          storage[3].hit,
-			CommentHits:        storage[4].hit,
-			AttributeHits:      storage[5].hit,
-			AttributeValueHits: storage[6].hit,
+		Appear: HTMLNodeDiff{
+			TagStartHits:       storage.appear[0].hit,
+			TagEndHits:         storage.appear[1].hit,
+			TagSelfCloseHits:   storage.appear[2].hit,
+			WordsHits:          storage.appear[3].hit,
+			CommentHits:        storage.appear[4].hit,
+			AttributeHits:      storage.appear[5].hit,
+			AttributeValueHits: storage.appear[6].hit,
 			HTMLNode: httpprepare.HTMLNode{
-				TagStart:       storage[0].data,
-				TagEnd:         storage[1].data,
-				TagSelfClose:   storage[2].data,
-				Words:          storage[3].data,
-				Comment:        storage[4].data,
-				Attribute:      storage[5].data,
-				AttributeValue: storage[6].data,
+				TagStart:       storage.appear[0].data,
+				TagEnd:         storage.appear[1].data,
+				TagSelfClose:   storage.appear[2].data,
+				Words:          storage.appear[3].data,
+				Comment:        storage.appear[4].data,
+				Attribute:      storage.appear[5].data,
+				AttributeValue: storage.appear[6].data,
+			},
+		},
+		Disappear: HTMLNodeDiff{
+			TagStartHits:       storage.disappear[0].hit,
+			TagEndHits:         storage.disappear[1].hit,
+			TagSelfCloseHits:   storage.disappear[2].hit,
+			WordsHits:          storage.disappear[3].hit,
+			CommentHits:        storage.disappear[4].hit,
+			AttributeHits:      storage.disappear[5].hit,
+			AttributeValueHits: storage.disappear[6].hit,
+			HTMLNode: httpprepare.HTMLNode{
+				TagStart:       storage.disappear[0].data,
+				TagEnd:         storage.disappear[1].data,
+				TagSelfClose:   storage.disappear[2].data,
+				Words:          storage.disappear[3].data,
+				Comment:        storage.disappear[4].data,
+				Attribute:      storage.disappear[5].data,
+				AttributeValue: storage.disappear[6].data,
 			},
 		},
 	}
 }
 
-func (diff *Difference) GetHeadersDiff() HeaderResult {
-	return HeaderResult{}
-}
+// Take two prepared header structures and compare their differences
+func (diff *Difference) GetHeadersDiff(HeaderNode httpprepare.Header) HeaderResult {
+	var (
+		appear      = httpprepare.NewHeader()
+		disappear   = httpprepare.NewHeader()
+		testedItems = make(map[string]struct{})
+		totalHits   = 0
+	)
 
-/*
-	func lstDiff(lst1, lst2 []string) []string {
-		diff := []string{}
-		set := make(map[string]bool)
+	for currentHeader, currentHeaderData := range HeaderNode {
 
-		// Populate set with elements from list2
-		for _, elem := range lst2 {
-			set[elem] = true
+		// Check if the information is unique in relation to the shared header names
+		if knownHeaderData, ok := diff.Compare.HeaderMergeNode[currentHeader]; ok {
+
+			// Add to tested header names
+			testedItems[currentHeader] = struct{}{}
+
+			// Search for unique values inside the current and known header data values
+			if (!lstIntShareItem(knownHeaderData.Amount, currentHeaderData.Amount) ||
+				!lstStringShareItem(knownHeaderData.Values, currentHeaderData.Values)) &&
+				!slices.Contains(knownHeaderData.Values, diff.Payload) {
+
+				appear[currentHeader] = currentHeaderData
+			}
+		} else {
+			appear[currentHeader] = currentHeaderData
 		}
+	}
 
-		// Check each element in list1 against the set
-		for _, elem := range lst1 {
-			if !set[elem] {
-				diff = append(diff, elem)
+	// Add headers that disappear in the current response compare to the original
+	for knownHeader, knownHeaderData := range diff.Compare.HeaderMergeNode {
+		if _, ok := testedItems[knownHeader]; !ok {
+			// Extract the highest difference from the known values and add it
+			disappear[knownHeader] = httpprepare.HeaderInfo{
+				Amount: []int{highestLstIntValue(knownHeaderData.Amount)},
+				Values: knownHeaderData.Values,
 			}
 		}
-		return diff
+
 	}
-*/
-func nodeDiff(current diffNode, known map[string][]int, payload string) diffNode {
-	diffhtmlNode := newDiffNode()
+	totalHits = len(disappear) + len(appear)
+	return HeaderResult{
+		OK:         (totalHits > 0),
+		HeaderHits: totalHits,
+		Appear:     appear,
+		Disappear:  disappear,
+	}
+}
+
+func nodeDiff(current diffNode, known map[string][]int, payload string) (diffNode, diffNode) {
+	var (
+		appear      = newDiffNode()
+		disappear   = newDiffNode()
+		testedItems = make(map[string]struct{})
+	)
 
 	for currentItem, currentValue := range current.data {
-		// Set the diff as true by default
-		diff := true
+		// Set the isDiff as true by default
+		isDiff := true
 		amountDiff := 0
 
 		// Check if the current item exists in the known
 		if knownValues, ok := known[currentItem]; ok {
+
+			// Add the item to the tested map to be used to discover items that disappear in the response body
+			testedItems[currentItem] = struct{}{}
+
 			// Compare with the known values
 			for _, knownValue := range knownValues {
 				if currentValue == knownValue {
-					diff = false
+					isDiff = false
 					break
 
 					// If the current value isen't in the list check the amount diff and update to the highest amount diff
@@ -175,18 +237,37 @@ func nodeDiff(current diffNode, known map[string][]int, payload string) diffNode
 			amountDiff = currentValue
 		}
 
-		// FIX THIS
-		if diff && currentItem != payload {
-			// Check false positive
+		if isDiff && currentItem != payload {
+			// Check randomness (false positive)
 			if !current.checkRandomness || (current.checkRandomness && !randomness.IsRandom(currentItem, true)) {
-				diffhtmlNode.data[currentItem] = amountDiff
-				diffhtmlNode.hit += amountDiff
+				appear.data[currentItem] = amountDiff
+				appear.hit += amountDiff
 			}
 		}
 	}
 
-	//fmt.Println("DIFF", payload, diffhtmlNode) //DEBUG - REMOVE
-	return diffhtmlNode
+	// Check known item and see if any of them where not included in the current response, then add them as a valid diff
+	for knownItem, knownValues := range known {
+		if _, ok := testedItems[knownItem]; !ok {
+			// Check randomness (false positive)
+			if !current.checkRandomness || (current.checkRandomness && !randomness.IsRandom(knownItem, true)) {
+				value := highestLstIntValue(knownValues)
+				disappear.data[knownItem] = value
+				disappear.hit += value
+			}
+		}
+	}
+	return appear, disappear
+}
+
+func highestLstIntValue(lst []int) int {
+	v := 0
+	for _, i := range lst {
+		if i > v {
+			v = i
+		}
+	}
+	return v
 }
 
 // Return an int array of 2 that holds 0/1 (min/max) and length diff
@@ -195,4 +276,42 @@ func lengthMinMaxDiff(x, y int) int {
 		return (y - x)
 	}
 	return x - y
+}
+
+// Compare two int type lists and return the differences presented in lstCurrent
+// Return a true if the lists has one item in common
+func lstIntShareItem(lstCompare, lstCurrent []int) bool {
+	// Convert list1 into a map for quick lookups
+	m := make(map[int]struct{})
+	for _, i := range lstCompare {
+		m[i] = struct{}{}
+	}
+
+	// Iterate over list2 and check if any item exists in the map
+	for _, i := range lstCurrent {
+		// An item is shared between the two lists
+		if _, ok := m[i]; ok {
+			return true
+		}
+	}
+	return false
+}
+
+// Compare two string type lists and return the differences presented in lstCurrent
+// Return a true if the lists has one item in common
+func lstStringShareItem(lstCompare, lstCurrent []string) bool {
+	// Convert list1 into a map for quick lookups
+	m := make(map[string]struct{})
+	for _, i := range lstCompare {
+		m[i] = struct{}{}
+	}
+
+	// Iterate over list2 and check if any item exists in the map
+	for _, i := range lstCurrent {
+		// An item is shared between the two lists
+		if _, ok := m[i]; ok {
+			return true
+		}
+	}
+	return false
 }
