680e6ae3fc40a8122a2a688bacdef2b9530a3ae4
brumens <68474266+Brum3ns@users.noreply.github.com>
2025-06-24 22:05:10 +0200

    updated firefly

--- cmd/firefly/firefly.go
+++ cmd/firefly/firefly.go
@@ -5,29 +5,42 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"time"
 
-	"github.com/Brum3ns/firefly/internal/banner"
-	"github.com/Brum3ns/firefly/internal/config"
-	"github.com/Brum3ns/firefly/internal/global"
 	"github.com/Brum3ns/firefly/internal/option"
 	"github.com/Brum3ns/firefly/internal/runner"
 	"github.com/Brum3ns/firefly/internal/setup"
 	"github.com/Brum3ns/firefly/pkg/design"
+	"github.com/Brum3ns/firefly/pkg/faker"
 )
 
 func main() {
 	//Check resources before starting (first time use):
 	if _, err := setup.Setup(); err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
 	//Setup user arguments (options)
-	opt := option.NewOptions()
+	opt, err := option.NewOption()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if opt.ListPlaceholders {
+		faker.PrintList()
+		return
+	}
+
+	if opt.TestPlaceholders != "" {
+		result, err := faker.Generate(opt.TestPlaceholders)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(result)
+		return
+	}
 
 	// Configure user arguments (options)
-	conf, err := config.NewConfigure(opt)
+	/* conf, err := config.NewConfigure(opt)
 	if err != nil {
 		log.Fatal(design.STATUS.ERROR, err)
 	}
@@ -35,7 +48,7 @@ func main() {
 	if !conf.Option.TerminalUI {
 		banner.Banner()
 		banner.Disclaimer()
-	}
+	} */
 
 	//Listen for user keypress (CTRL + C):
 	c := make(chan os.Signal, 1)
@@ -47,24 +60,44 @@ func main() {
 		}
 	}()
 
-	timer := time.Now()
+	// Run the runner
+	r, err := runner.NewRunner(opt)
+	if err != nil {
+		log.Fatalf("failed to create runner instance, error : %v", err)
+	}
+
+	if err := r.FetchKnowledge(); err != nil {
+		log.Fatalln(err)
+	}
+
+	//if !r.HasKnowledge() {
+	//	fmt.Println("No knowledge could be gather from the target")
+	//	return
+	//}
+
+	stats, err := r.RunFuzz()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(stats)
 
 	//Run the runner in verifyication process mode to detect normal behavior and patterns within the target:
-	VerifyRunner := runner.NewRunner(conf, nil)
+	/* VerifyRunner := runner.NewRunner(conf, nil)
 	KnowledgeStorage, _, err := VerifyRunner.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//Run the black-box enumiration process:
-	AttackRunner := runner.NewRunner(conf, KnowledgeStorage)
-	_, Statistic, err := AttackRunner.Run()
+	r := runner.NewRunner(conf)
+	_, Statistic, err := r.Run()
 	if err != nil {
 		log.Fatal(err)
-	}
+	}*/
 
 	//Display summary of the process:
-	fmt.Printf(
+	/* fmt.Printf(
 		"%s\033[1;32m\u2713\033[0m Process finished: Requests/Responses:[%d/%d], Scanned:[\033[1;32m%d\033[0m], Behavior:[\033[1;33m%d\033[0m], Filtered:[\033[1;36m%d\033[0m], Error:[\033[31m%d\033[0m], Time:[%v]\n",
 		global.TERMINAL_CLEAR,
 		Statistic.Request.GetCount(),
@@ -74,5 +107,5 @@ func main() {
 		Statistic.Request.GetFilterCount(),
 		Statistic.Request.GetErrorCount(),
 		time.Since(timer),
-	)
+	) */
 }
