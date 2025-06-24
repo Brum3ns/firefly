12572dc337cf2d07b6a0bc05d6f5ada26b55af58
brumens <68474266+Brum3ns@users.noreply.github.com>
2025-06-24 21:49:15 +0200

    changed color

--- pkg/design/style.go
+++ pkg/design/style.go
@@ -6,7 +6,7 @@ const (
 	COLOR_RED    = lipgloss.Color("#EB2D3A")
 	COLOR_BLUE   = lipgloss.Color("#0069D9")
 	COLOR_PINK   = lipgloss.Color("#D900C7")
-	COLOR_GREY   = lipgloss.Color("#383838")
+	COLOR_GREY   = lipgloss.Color("#555555")
 	COLOR_WHITE  = lipgloss.Color("#D9DCCF")
 	COLOR_BLACK  = lipgloss.Color("#000000")
 	COLOR_GREEN  = lipgloss.Color("#3AF191")
