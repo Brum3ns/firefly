c633553b23b94791d5db9e04e1db103c4fa06fcc
brumens <68474266+Brum3ns@users.noreply.github.com>
2025-06-24 21:57:03 +0200

    new structure output

--- internal/output/result.go
+++ internal/output/result.go
@@ -1,42 +1,28 @@
 package output
 
-import (
-	"net/http"
-
-	"github.com/Brum3ns/firefly/pkg/extract"
-	"github.com/Brum3ns/firefly/pkg/httpdiff"
-	"github.com/Brum3ns/firefly/pkg/httpreflect"
-	"github.com/Brum3ns/firefly/pkg/transformation"
-)
+import "github.com/Brum3ns/firefly/pkg/rhttp"
 
 // The output result is the final result that is generated that stores all the result from all processes done by the runner
 // Note : (The final result only stores the result details that are of int `json:""`erest to the user and not the properties that were used during the runner process. The variable may be reformulated for better readability)
-type ResultFinal struct {
-	RequestId      int      `json:"RequestId"`
-	TargetHashId   string   `json:"TargetId"`
-	Tag            string   `json:"Tag"`
-	Date           string   `json:"Date"`
-	Payload        string   `json:"Payload"`
-	Request        Request  `json:"Request"`
-	Response       Response `json:"Response"`
-	Scanner        Scanner  `json:"Scanner"`
-	Error          error    `json:"Error"`
-	OK             bool     `json:"-"`
-	UnkownBehavior bool
+type Result struct {
+	RequestId    int            `json:"RequestId"`
+	TargetHashId string         `json:"TargetId"`
+	Tag          string         `json:"Tag"`
+	Date         string         `json:"Date"`
+	Payload      string         `json:"Payload"`
+	Request      rhttp.Http     `json:"Request"`
+	Response     rhttp.Response `json:"Response"`
+	//Scanner        Scanner  `json:"Scanner"`
+	Error error `json:"Error"`
+	OK    bool  `json:"-"`
+	//UnkownBehavior bool
 	//Origin       string   `json:"Origin"`
 	//Behavior     Behavior `json:"Behavior"`
 }
 
-//TODO
-// Contains the collected Behavior from the target
-/* type Behavior struct {
-	CWE         string `json:"CWE"`
-	Component   string `json:"Component"`
-	Confidence  string `json:"Confidence"`
-	Description string `json:"Desc"`
-} */
-
 // Refer to the results of the Request/response process
+
+/*
 type Request struct {
 	URL         string      `json:"URL"`
 	URLOriginal string      `json:"URL-Original"`
@@ -73,3 +59,4 @@ type Scanner struct {
 	HttpReflectExtracts     []string                  `json:"HttpReflectExtracts"`
 	HttpReflectSurroundings []httpreflect.Surrounding `json:"HttpReflectSurroundings"`
 }
+*/
